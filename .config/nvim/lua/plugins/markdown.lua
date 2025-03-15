1c8efae915cc1e0fbc2d6d6104869869e16038d1
partha996 <parth99681@gmail.com>
2025-03-16 05:26:50 +0530

    remove useless comments

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,12 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
+	--@module 'render-markdown'
+	config = function()
+		require("render-markdown").setup({
+			enabled = false,
+			preset = "round",
+			ft = { "markdown", "quarto" },
+		})
+	end,
 }
