f785845fea80b0d7fc41e16fe441ea54f055c758
partha996 <parth99681@gmail.com>
2025-03-16 05:14:00 +0530

    exclude zsh,tmux etc config files

--- .config/nvim/lua/plugins/virtColumn.lua
+++ .config/nvim/lua/plugins/virtColumn.lua
@@ -2,7 +2,7 @@ return {
 	"lukas-reineke/virt-column.nvim",
 	config = function()
 		require("virt-column").setup({
-			exclude = { filetypes = { "markdown", "txt", "pdf" } },
+			exclude = { filetypes = { "markdown", "txt", "pdf", "zsh", "tmux" } },
 			update = true,
 			char = "|",
 			virtcolumn = "+1,120",
