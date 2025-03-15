ccc037ea6994694d4ed7b437012191817cadcf8c
partha996 <parth99681@gmail.com>
2025-03-15 16:17:40 +0530

    moved dealwithit,cellular,beacon together

---
+++ .config/nvim/lua/plugins/looks.lua
@@ -0,0 +1,10 @@
+return {
+	"AndrewRadev/dealwithit.vim",
+	"eandrju/cellular-automaton.nvim",
+	"danilamihailov/beacon.nvim",
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		config = true,
+		event = { "winLeave" },
+	},
+}
