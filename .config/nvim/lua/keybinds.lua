890873d60b098c2a08a192cf979838fbfbe0c62e
partha996 <parth99681@gmail.com>
2025-08-25 00:23:49 +0530

    added keybind for rename lsp stuffs and add navbuddy keybinds and minor changes

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -5,12 +5,7 @@ vim.keymap.set("n", "<leader>q", "<Cmd>q<CR>", { desc = "close buffer" })
 vim.keymap.set("n", "<leader>o", "<Cmd>call append(line('.'), '')<CR>", { desc = "add empty line below" })
 vim.keymap.set("n", "<leader>O", "<Cmd>call append(line('.') - 1, '')<CR>", { desc = "add empty line above" })
 vim.keymap.set("n", "<leader>j", '"_d', { desc = "permanently delete motion" })
-
--- Navigating between windows
-vim.keymap.set("n", "<leader>wh", "<Cmd>wincmd h<CR>", { desc = "foucus rightwin" })
-vim.keymap.set("n", "<leader>wj", "<Cmd>wincmd j<CR>", { desc = "focus downwin" })
-vim.keymap.set("n", "<leader>wk", "<Cmd>wincmd k<CR>", { desc = "focus upwin" })
-vim.keymap.set("n", "<leader>wl", "<Cmd>wincmd l<CR>", { desc = "foucus leftwin" })
+vim.keymap.set("n", "gx", [[:silent execute '!zen-browser ' . shellescape(expand('<cfile>'), 1)<CR>]])
 
 -- Split windows
 vim.keymap.set("n", "<leader>vs", "<Cmd>vsplit<CR>", { desc = "vertical split" })
@@ -25,7 +20,13 @@ vim.keymap.set("n", "<leader>tc", "<Cmd>tabn<CR>", { desc = "cycle tabs" })
 vim.keymap.set("n", "<leader>hv", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>cs", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>mp", vim.lsp.buf.format, {})
+vim.keymap.set("n","<leader>rt",vim.lsp.buf.rename, {})
+
+-- Lsp symbol Navbuddy window
+vim.keymap.set("n", "<leader>nv", "<cmd>Navbuddy<CR>", { desc = "Toggle Outline" })
+
+-- code formatter
+vim.keymap.set("n", "<leader>mp", function()require("conform").format()end)
 
 -- Yank the whole buffer
 vim.keymap.set("n", "<leader>cb", "<Cmd>normal! ggVGy<C-o><CR>", { silent = true })
@@ -33,90 +34,99 @@ vim.keymap.set("n", "<leader>cb", "<Cmd>normal! ggVGy<C-o><CR>", { silent = true
 -- Delete buffer from the memory
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete current buffer" })
 
--- Diagnostic jumps
-vim.keymap.set("n", "<leader>du", function()
-	vim.diagnostic.goto_prev({ float = false })
-end, { desc = "Go to previous diagnostic without popup" })
+-- Diagnostic jumps up
+vim.keymap.set("n", "<leader>du", function()vim.diagnostic.goto_prev({ float = false })end, { desc = "Go to previous diagnostic" })
 
-vim.keymap.set("n", "<leader>dd", function()
-	vim.diagnostic.goto_next({ float = false })
-end, { desc = "Go to next diagnostic without popup" })
+-- Diagnostic jumps down
+vim.keymap.set("n", "<leader>dd", function()vim.diagnostic.goto_next({ float = false })end, { desc = "Go to next diagnostic" })
 
--- Snacks keybinds
-vim.keymap.set("n", "<leader>lg", function()
-	require("snacks").lazygit()
-end, { desc = "lazygit" })
+-- Define snacks
+local snacks = require("snacks")
 
-vim.keymap.set("n", "<leader>gz", function()
-	require("snacks").gitbrowse()
-end, { desc = "gitbrowser" })
+-- Snacks.lazygit
+vim.keymap.set("n", "<leader>lg", function()snacks.lazygit()end, { desc = "lazygit" })
 
-vim.keymap.set("n", "<leader>un", function()
-	require("snacks").notifier.hide()
-end, { desc = "hide notifications" })
+-- Snacks.gitbrowse
+vim.keymap.set("n", "<leader>gz", function()snacks.gitbrowse()end, { desc = "gitbrowser" })
 
-vim.keymap.set("n", "<leader>gb", function()
-	require("snacks").git.blame_line()
-end, { desc = "git blame" })
+-- Snacks.notifier hide
+vim.keymap.set("n", "<leader>un", function()snacks.notifier.hide()end, { desc = "hide notifications" })
 
-vim.keymap.set("n", "<leader>rb", function()
-	require("snacks").rename.rename_file()
-end, { desc = "reaname current buffer" })
+-- Snacks.git blame_line
+vim.keymap.set("n", "<leader>gb", function()snacks.git.blame_line()end, { desc = "git blame" })
 
-vim.keymap.set("n", "<leader>sl", function()
-	require("snacks").picker.lsp_symbols({ layout = "right" })
-end, { desc = "treesitter" })
+-- Snacks.git log
+vim.keymap.set("n", "<leader>gl", function()snacks.picker.git_log()end)
 
-vim.keymap.set("n", "<leader>ff", function()
-	require("snacks").picker.files()
-end, { desc = "file picker" })
+-- Snacks.rename_file
+vim.keymap.set("n", "<leader>rb", function()snacks.rename.rename_file()end, { desc = "reaname current buffer" })
 
-vim.keymap.set("n", "<leader>zf", function()
-	require("snacks").picker.zoxide()
-end, { desc = "zoxide search " })
+-- Snacks.picker lsp_symbols
+vim.keymap.set("n", "<leader>sl", function()snacks.picker.lsp_symbols({ layout = "right" })end, { desc = "treesitter" })
 
-vim.keymap.set("n", "<leader>sb", function()
-	require("snacks").picker.buffers()
-end, { desc = "find open buffers" })
+-- Snacks.picker smart_file
+vim.keymap.set("n", "<leader>ff", function()snacks.picker.files({ layout = "telescope" })end, { desc = "file picker" })
 
-vim.keymap.set("n", "<leader>rg", function()
-	require("snacks").picker.registers()
-end, { desc = "registers" })
+-- Snacks.picker zoxide
+vim.keymap.set("n", "<leader>zf", function()snacks.picker.zoxide()end, { desc = "zoxide search " })
 
-vim.keymap.set("n", "<leader>ch", function()
-	require("snacks").picker.command_history({ layout = "select" })
-end, { desc = "command history" })
+-- Snacks.picker buffers
+vim.keymap.set("n", "<leader>sb", function()snacks.picker.buffers({ layout = "select" })end, { desc = "find open buffers" })
 
-vim.keymap.set("n", "<leader>ms", function()
-	require("snacks").picker.man()
-end, { desc = "man pages" })
+-- Snacks.picker register
+vim.keymap.set("n", "<leader>rg", function()snacks.picker.registers()end, { desc = "registers" })
 
-vim.keymap.set("n", "<leader>kp", function()
-	require("snacks").picker.keymaps({ layout = "ivy" })
-end, { desc = "keymaps picker" })
+-- Snacks.picker command_history
+vim.keymap.set("n", "<leader>ch", function()snacks.picker.command_history({ layout = "select" })end, { desc = "command history" })
 
-vim.keymap.set("n", "<leader>up", function()
-	require("snacks").picker.undo({ layout = "ivy" })
-end, { desc = "change history recorder" })
+-- Snacks.picker man
+vim.keymap.set("n", "<leader>ms", function()snacks.picker.man()end, { desc = "man pages" })
 
-vim.keymap.set("n", "<leader>ss", function()
-	require("snacks").picker.spelling({ layout = "select" })
-end, { desc = "word picker" })
+-- Snacks.picker keymaps
+vim.keymap.set("n", "<leader>kp", function()snacks.picker.keymaps({ layout = "ivy" })end, { desc = "keymaps picker" })
 
-vim.keymap.set("n", "<leader>sm", function()
-	require("snacks").picker.marks()
-end, { desc = "marks" })
+-- Snacks.picker undo
+vim.keymap.set("n", "<leader>ut", function()snacks.picker.undo({ layout = "ivy" })end, { desc = "change history recorder" })
 
+-- Snacks.picker spelling
+vim.keymap.set("n", "<leader>ss", function()snacks.picker.spelling({ layout = "select" })end, { desc = "word picker" })
+
+-- Snacks.picker marks
+vim.keymap.set("n", "<leader>sm", function()snacks.picker.marks()end, { desc = "marks" })
+
+-- Snacks.picker grep
+vim.keymap.set("n", "<leader>vp", function()snacks.picker.marks({layout = "telescope"})end, { desc = "grep in the cwd" })
+
+-- Snacks.picker grep_buffers
+vim.keymap.set("n", "<leader>gp", function()snacks.picker.grep_buffers()end, { desc = "grep inside of only open buffers" })
+
+-- Snacks.words jump
+vim.keymap.set("n", "[[", function()snacks.words.jump(vim.v.count1)end)
+vim.keymap.set("n", "]]", function()snacks.words.jump(-vim.v.count1)end)
+
+-- Scratchpad
+-- vim.keymap.set("n", "<leader>sc", "<Cmd>lua Snacks.scratch()<CR>")
+
+-- File explorer(snacks's picker)
+vim.keymap.set("n", "<leader>e", "<Cmd>lua Snacks.explorer()<CR>", { desc = "open file explorer" })
+
+-- notifications
+vim.keymap.set("n","<leader>sn","<Cmd>lua Snacks.picker.notifications()<CR>", { desc = "show notifictions in picker" })
 
 -- Make current buffer executable
 vim.keymap.set("n", "<leader>mx", "<Cmd>!chmod +x %<CR>", { desc = "make file executable" })
 
--- File explorer(snacks's picker)
-vim.keymap.set("n", "<leader>e", "<Cmd>lua Snacks.explorer()<CR>", { desc = " open file explorer" })
-
 -- Ufo fold
 vim.keymap.set("n", "<leader>fo", require("ufo").openAllFolds)
 vim.keymap.set("n", "<leader>fc", require("ufo").closeAllFolds)
+
+-- Peek inside a fold
+vim.keymap.set("n", "zk", function()
+	local winid = require("ufo").peekFoldedLinesUnderCursor()
+	if not winid then
+		vim.lsp.buf.hover()
+	end
+end, { desc = "peek inside the fold" })
 
 -- Resize panes with Ctrl + Arrow Keys
 -- vim.keymap.set("n", "<C-Up>", "<C-w>+", { desc = "Increase pane height" })
@@ -134,35 +144,16 @@ vim.keymap.set("n", "<leader>cu", "<Cmd>lua MiniBracketed.comment('backward')<CR
 vim.keymap.set("n", "<Leader>xd", "<Cmd>lua MiniBracketed.conflict('forward')<CR>", { desc = "jump conflict up" })
 vim.keymap.set("n", "<Leader>xu", "<Cmd>lua MiniBracketed.conflict('backward')<CR>", { desc = "jump conflict down" })
 
--- Trial space remover
-vim.keymap.set("n", "<Leader>tr", "<Cmd>lua MiniTrailspace.trim()<CR>", { desc = "Remove tralling spaces " })
+--Mini Mics window fullscreen
+vim.keymap.set("n", "<Leader>fw", function()require("mini.misc").zoom()end, { desc = "make current focus window fullscreen" })
 
--- Useless but cool animation
-vim.keymap.set(
-	"n",
-	"<leader>ca",
-	"<Cmd>CellularAutomaton make_it_rain<CR>",
-	{ noremap = true, silent = true },
-	{ desc = "cool animation" }
-)
+-- Useless cool animation
+vim.keymap.set("n", "<leader>ca", "<Cmd>CellularAutomaton make_it_rain<CR>", { noremap = true, silent = true })
 
--- Toggle terminal
-vim.keymap.set("n", "<C-g>", "<Cmd>ToggleTerm<CR>")
-vim.keymap.set("t", "<C-g>", "<Cmd>ToggleTerm<CR>")
 
 -- Trouble
-vim.keymap.set("n", "<leader>st", "<Cmd>Trouble diagnostics focus<CR>", { desc = "Diagnostics (Trouble)" })
+vim.keymap.set(	"n","<leader>st","<Cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Diagnostics (Trouble)" })
 
--- Navbuddy
-vim.keymap.set("n", "<leader>so", "<Cmd>Navbuddy<CR>", { desc = "Open navbuddy" })
-
--- Undotree
-vim.keymap.set(
-	"n",
-	"<leader>ut",
-	"<Cmd>UndotreeToggle<CR>|<Cmd>UndotreeFocus<CR>",
-	{ desc = "Toggle Undotree & focus" }
-)
 
 -- Rendering Markdown files
 vim.keymap.set("n", "<leader>am", "<Cmd>Markview Toggle<CR>", { desc = "toggle markdown renderings " })
@@ -170,3 +161,5 @@ vim.keymap.set("n", "<leader>am", "<Cmd>Markview Toggle<CR>", { desc = "toggle m
 -- Debugger
 vim.keymap.set("n", "<leader>dt", require("dap").toggle_breakpoint, {})
 vim.keymap.set("n", "<leader>dc", require("dap").continue, {})
+vim.keymap.set("n", "<leader>di", "<Cmd>DapStepInto<Cr>", {})
+vim.keymap.set("n", "<leader>do", "<Cmd>DapStepOut<Cr>", {})
