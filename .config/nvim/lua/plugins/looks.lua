8d1098a02ee5c7aea9b09f316230b4927dc18ec4
partha996 <parth99681@gmail.com>
2025-08-25 00:26:07 +0530

    remove bacon

--- .config/nvim/lua/plugins/looks.lua
+++ .config/nvim/lua/plugins/looks.lua
@@ -1,10 +1,14 @@
 return {
-	"AndrewRadev/dealwithit.vim",
 	"eandrju/cellular-automaton.nvim",
-	"danilamihailov/beacon.nvim",
 	{
 		"nvim-zh/colorful-winsep.nvim",
 		config = true,
 		event = { "winLeave" },
+		-- require("colorful-winsep").setup({
+		-- 	hi = {
+		-- 		bg = "#B97EC8",
+		-- 		fg = "#1F3442",
+		-- 	},
+		-- }),
 	},
 }
